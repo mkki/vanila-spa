ebbfd5f8460613139ba8b20b0b59bbe690c754bd
mkki <devmkki@gmail.com>
2025-06-13 01:44:59 +0900

    ViewModel이 상태를 직접 관리하도록 수정

--- src/core/ViewModel.ts
+++ src/core/ViewModel.ts
@@ -2,14 +2,29 @@ type Subscriber<T> = (state: T) => void;
 
 export abstract class ViewModel<T> {
   private subscribers: Subscriber<T>[] = [];
+  protected state: T;
+
+  constructor(initialState: T) {
+    this.state = initialState;
+  }
 
   subscribe(callback: Subscriber<T>): void {
     this.subscribers.push(callback);
+    callback(this.state);
   }
 
-  protected notify(payload: T): void {
+  protected notify(): void {
     for (const callback of this.subscribers) {
-      callback(payload);
+      callback(this.state);
     }
+  }
+
+  getState(): T {
+    return this.state;
+  }
+
+  setState(newState: T): void {
+    this.state = newState;
+    this.notify();
   }
 }
--- src/viewmodel/TodoViewModel.ts
+++ src/viewmodel/TodoViewModel.ts
@@ -2,24 +2,19 @@ import { ViewModel } from '@/core/ViewModel';
 import type { Todo } from '@/model/TodoModel';
 
 export class TodoViewModel extends ViewModel<Todo[]> {
-  private todos: Todo[];
-
   constructor(todos: Todo[]) {
-    super();
-    this.todos = todos;
+    super(todos);
   }
 
   toggleCompleted(index: number) {
-    this.todos[index].completed = !this.todos[index].completed;
-    this.notify(this.todos);
+    if (index < 0 || index >= this.state.length) {
+      throw new Error('Index out of bounds');
+    }
+    this.state[index].completed = !this.state[index].completed;
+    this.notify();
   }
 
   subscribe(callback: (todos: Todo[]) => void): void {
     super.subscribe(callback);
-    callback(this.todos);
-  }
-
-  getTodos(): Todo[] {
-    return this.todos;
   }
 }
